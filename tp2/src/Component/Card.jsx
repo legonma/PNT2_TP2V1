6ae5fa02df3e36111ca38a53c4bd6afa9064f943
legonma <marianolegon@gmail.com>
2023-09-19 18:57:10 -0300

    add new styles and loop

--- tp2/src/Component/Card.jsx
+++ tp2/src/Component/Card.jsx
@@ -3,29 +3,35 @@ import axios from "axios";
 import './Card.css';
 
 export default function Card() {
+    //initialize
     const [card, setCard] = useState('CardFlipped')
+    const [character, setCharacter] = useState(null);
+
+    //handler
     const flipCard = () => {
         card === 'Card' ? setCard('CardFlipped') : setCard('Card');
     }   
-    const [character, setCharacter] = useState(null);
 
 
     useEffect(() => {
         const fetchCharacter = async () => {
             try {
                 const response = await axios.get('https://rickandmortyapi.com/api/character');
-                const randomCharacter = returnCharacter(response);
-                setCharacter(randomCharacter);
+                pickupCharacter(response);
             } catch (error) {
                 console.error(error);
             }
         };
 
-        const returnCharacter = (response) => {
+        function pickupCharacter(response) {
             const length = response.data.results.length;
-            let char = response.data.results[Math.floor(Math.random() * length) - 1]
-            return char;
+            let char;
+            do {
+                char = response.data.results[Math.floor(Math.random() * length)]
+            } while (char.id === 19);
+            setCharacter(char);
         }
+
         fetchCharacter();
     }, []);
 
@@ -36,10 +42,10 @@ export default function Card() {
                     <h2 className = "Name">{character && character.name}</h2>
                     <img className = "img" src={character && character.image}/>
                     <div className = "Attr_Container">
-                        <p className = "Attr">Spacies: {character && character.species}</p>
-                        <p className = "Attr">Location: {character && character.location.name}</p>
-                        <p className = "Attr">Status: {character && character.status}</p>
-                        <p className = "Attr">Total Episodie {character && character.episode.length}</p>
+                        <p className = "Attr"><b>Spacies:</b> {character && character.species}</p>
+                        <p className = "Attr"><b>Location:</b> {character && character.location.name}</p>
+                        <p className = "Attr"><b>Status:</b> {character && character.status}</p>
+                        <p className = "Attr"><b>Total Episodie:</b> {character && character.episode.length}</p>
                     </div>
 
                 </div>
